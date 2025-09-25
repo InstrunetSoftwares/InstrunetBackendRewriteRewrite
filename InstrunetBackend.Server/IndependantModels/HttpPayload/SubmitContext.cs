33b6e2a549e5685391dcd0d38a5e6fdc64cb09ee
AXCWG <xiey0@qq.com>
2025-09-26 03:32:58 +0800

    Release 2.0

--- InstrunetBackend.Server/IndependantModels/HttpPayload/SubmitContext.cs
+++ InstrunetBackend.Server/IndependantModels/HttpPayload/SubmitContext.cs
@@ -5,7 +5,7 @@ public class SubmitContext<IFormFile>
     public required string name { get; set;  }
     public string? albumName { get; set;  }
     public string? artist { get; set;  }
-    public string? albumCover { get; set;  }
+    public IFormFile? albumCover { get; set;  }
     public string? link { get; set;  }
     public required IFormFile fileBinary { get; set;  }
     public string? email { get; set;  }
