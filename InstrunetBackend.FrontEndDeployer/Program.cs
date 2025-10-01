750ddfabfd509c3cb0f9955619ca98a16af99973
AXCWG <xiey0@qq.com>
2025-10-02 04:06:28 +0800

    deployer...?

---
+++ InstrunetBackend.FrontEndDeployer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using LibGit2Sharp;
+
+using var repo = new Repository(".");
+if (!Directory.Exists(".git"))
+{
+    Console.WriteLine("No git repo found. ");
+    return;
+}
+
+while (true)
+{
+    var @operator = new Signature("AXCWG Bot", "xiey0@qq.com", DateTimeOffset.Now);
+
+    repo.MergeFetchedRefs(@operator, new MergeOptions()
+    {
+        FailOnConflict = true
+    });
+    var result = Commands.Pull(repo, @operator, new PullOptions());
+    if (result.Status == MergeStatus.FastForward)
+    {
+        var p = new Process();
+        p.StartInfo.FileName = "npm";
+        p.StartInfo.Arguments = "run build";
+        p.Start();
+        p.WaitForExit(); 
+        p.Dispose();
+        File.Copy("./web.config", "dist/web.config");
+    }
+}
