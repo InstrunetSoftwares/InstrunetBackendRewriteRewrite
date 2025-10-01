58aa5958fbb9e183d920f59dfba0ca6a762bf99e
AXCWG <xiey0@qq.com>
2025-10-02 04:23:20 +0800

    Deployer done

--- InstrunetBackend.FrontEndDeployer/Program.cs
+++ InstrunetBackend.FrontEndDeployer/Program.cs
@@ -10,21 +10,32 @@ if (!Directory.Exists(".git"))
 
 while (true)
 {
+    Console.WriteLine("Enabling operation..."); 
     var @operator = new Signature("AXCWG Bot", "xiey0@qq.com", DateTimeOffset.Now);
-
-    repo.MergeFetchedRefs(@operator, new MergeOptions()
+    Console.WriteLine("Now pulling...");
+    try
     {
-        FailOnConflict = true
-    });
-    var result = Commands.Pull(repo, @operator, new PullOptions());
-    if (result.Status == MergeStatus.FastForward)
-    {
-        var p = new Process();
-        p.StartInfo.FileName = "npm";
-        p.StartInfo.Arguments = "run build";
-        p.Start();
-        p.WaitForExit(); 
-        p.Dispose();
-        File.Copy("./web.config", "dist/web.config");
+        var result = Commands.Pull(repo, @operator, new PullOptions());
+        if (result.Status == MergeStatus.FastForward)
+        {
+            Console.WriteLine("Fast forwarding..."); 
+            var p = new Process();
+            p.StartInfo.UseShellExecute = true; 
+            p.StartInfo.FileName = "npm";
+            p.StartInfo.Arguments = "run build";
+            p.Start();
+            p.WaitForExit();
+            p.Dispose();
+            Console.WriteLine("Copying files...."); 
+            File.Copy("./web.config", "dist/web.config");
+            Console.WriteLine("Done copying files. "); 
+        }
     }
+    catch (Exception e)
+    {
+        Console.WriteLine(e); 
+    }
+   
+    Console.WriteLine("Waiting for another 15 sec. "); 
+    Task.Delay(TimeSpan.FromSeconds(15)).GetAwaiter().GetResult(); 
 }
