4cde651b466f1debe4b3df9ddeb838629c4ebbba
AXCWG <xiey0@qq.com>
2025-11-06 10:42:28 +0800

    npm i before process
    # Conflicts:
    #	.DS_Store

--- InstrunetBackend.FrontEndDeployer/Program.cs
+++ InstrunetBackend.FrontEndDeployer/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System.Diagnostics;
 using LibGit2Sharp;
 
+
 using var repo = new Repository(".");
+
 if (!Directory.Exists(".git"))
 {
     Console.WriteLine("No git repo found. ");
     return;
 }
+
+_ = Task.Run(() =>
+{
+    while (true)
+    {
+        var command = Console.ReadLine();
+        switch (command?.Trim())
+        {
+            case "exec":
+                Console.WriteLine("Enabling operation..."); 
+                var @operator = new Signature("AXCWG Bot", "xiey0@qq.com", DateTimeOffset.Now);
+                Console.WriteLine("Now pulling...");
+                try
+                {
+                    var result = Commands.Pull(repo, @operator, new PullOptions());
+                    if (result.Status == MergeStatus.FastForward)
+                    {
+                        Console.WriteLine("Fast forwarding..."); 
+                        var p = new Process();
+                        p.StartInfo.UseShellExecute = true; 
+                        p.StartInfo.FileName = "npm";
+                        p.StartInfo.Arguments = "run build";
+                        p.Start();
+                        p.WaitForExit();
+                        p.Dispose();
+                        Console.WriteLine("Copying files...."); 
+                        File.Copy("./web.config", "dist/web.config");
+                        Console.WriteLine("Done copying files. "); 
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e); 
+                }
+
+                break; 
+            default:
+                Console.WriteLine("Invalid command. Try again.");
+                break; 
+        }
+    }
+    // ReSharper disable once FunctionNeverReturns
+}); 
 
 while (true)
 {
@@ -18,16 +63,21 @@ while (true)
         var result = Commands.Pull(repo, @operator, new PullOptions());
         if (result.Status == MergeStatus.FastForward)
         {
-            Console.WriteLine("Fast forwarding..."); 
-            var p = new Process();
+            Console.WriteLine("Fast forwarding...");
+            using var npm_i = new Process();
+            npm_i.StartInfo.UseShellExecute = true;
+            npm_i.StartInfo.FileName = "npm";
+            npm_i.StartInfo.Arguments = "i"; 
+            npm_i.Start();
+            npm_i.WaitForExit(); 
+            using var p = new Process();
             p.StartInfo.UseShellExecute = true; 
             p.StartInfo.FileName = "npm";
             p.StartInfo.Arguments = "run build";
             p.Start();
             p.WaitForExit();
-            p.Dispose();
             Console.WriteLine("Copying files...."); 
-            File.Copy("./web.config", "dist/web.config");
+            File.Copy("./web.config", "dist/web.config", true);
             Console.WriteLine("Done copying files. "); 
         }
     }
